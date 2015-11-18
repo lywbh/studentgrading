253a459c82acb61e42deb6c501ce6fbc7d7a5518
Yifan Y Liao <lyyf927@qq.com>
2015-11-18 18:27:12 +0800

    inital project skeleton

---
+++ config/settings/common.py
@@ -0,0 +1,163 @@
+"""
+Django settings for mysite project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+from __future__ import absolute_import, unicode_literals
+
+import environ
+
+ROOT_DIR = environ.Path(__file__) - 3  # (/a/b/myfile.py - 3 = /)
+APPS_DIR = ROOT_DIR.path('studentgrading')
+
+env = environ.Env()
+environ.Env.read_env(str(ROOT_DIR) + '/.env')
+
+# APP CONFIGURATION
+# ------------------------------------------------------------------------------
+DJANGO_APPS = (
+    # Default Django apps:
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Useful template tags:
+    # 'django.contrib.humanize',
+
+    # Admin
+    'django.contrib.admin',
+)
+THIRD_PARTY_APPS = (
+    #'crispy_forms',  # Form layouts
+    #'allauth',  # registration
+    #'allauth.account',  # registration
+    #'allauth.socialaccount',  # registration
+)
+
+# Apps specific for this project go here.
+LOCAL_APPS = (
+    # Your stuff: custom apps go here
+    'studentgrading.polls',  # custom polls app
+)
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+# MIDDLEWARE CONFIGURATION
+# ------------------------------------------------------------------------------
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+# DEBUG
+# ------------------------------------------------------------------------------
+DEBUG = env.bool("DJANGO_DEBUG", False)
+
+# FIXTURE CONFIGURATION
+# ------------------------------------------------------------------------------
+'''
+FIXTURE_DIRS = (
+    str(APPS_DIR.path('fixtures')),
+)
+'''
+
+# EMAIL CONFIGURATION
+# ------------------------------------------------------------------------------
+# EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+
+# MANAGER CONFIGURATION
+# ------------------------------------------------------------------------------
+'''
+ADMINS = (
+    ("""Yifan Y Liao""", 'szlyyfgm@gmail.com'),
+)
+'''
+
+# MANAGERS = ADMINS
+
+# DATABASE CONFIGURATION
+# ------------------------------------------------------------------------------
+# environ does not support oracle db
+def get_db_setting_str(env_var):
+    if env(env_var).split(':')[0] == 'oracle':
+        setting = env.db(env_var, engine='django.db.backends.oracle')
+        setting['PORT'] = str(setting['PORT'])
+        return setting
+    return env.db(env_var)
+
+DATABASES = {
+    # oracle db setting
+    # 'default': env.db('DATABASE_URL', engine='django.db.backends.oracle')
+    'default': get_db_setting_str('DATABASE_URL')
+}
+
+# GENERAL CONFIGURATION
+# ------------------------------------------------------------------------------
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# In a Windows environment this must be set to your system time zone.
+TIME_ZONE = 'Asia/Shanghai'
+
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# TEMPLATE CONFIGURATION
+# ------------------------------------------------------------------------------
+TEMPLATES = [
+    {
+        'DIRS': [
+            str(APPS_DIR.path('templates')),
+        ],
+        'OPTIONS': {
+            'debug': DEBUG,
+        }
+    },
+]
+
+# STATIC FILE CONFIGURATION
+# ------------------------------------------------------------------------------
+STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    str(APPS_DIR.path('static')),
+)
+
+# MEDIA CONFIGURATION
+# ------------------------------------------------------------------------------
+MEDIA_ROOT = str(APPS_DIR('media'))
+
+MEDIA_URL = '/media/'
+
+# URL Configuration
+# ------------------------------------------------------------------------------
+ROOT_URLCONF = 'config.urls'
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+# AUTHENTICATION CONFIGURATION
+# ------------------------------------------------------------------------------
+# ...
+
+# Your common stuff: Below this line define 3rd party library settings
---
+++ config/settings/local_yifan.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+'''
+Local settings
+
+- Run in Debug mode
+- Add Django Debug Toolbar
+'''
+
+from config.settings.common import *
+
+# DEBUG
+# ------------------------------------------------------------------------------
+DEBUG = env.bool('DJANGO_DEBUG', default=True)
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
+
+# SECRET CONFIGURATION
+# ------------------------------------------------------------------------------
+# Note: This key only used for development and testing.
+SECRET_KEY = env("SECRET_KEY")
+
+# Mail settings
+# ------------------------------------------------------------------------------
+#...
+
+# CACHING
+# ------------------------------------------------------------------------------
+
+# django-debug-toolbar
+# ------------------------------------------------------------------------------
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+INSTALLED_APPS += ('debug_toolbar', )
+
+# TESTING
+# ------------------------------------------------------------------------------
