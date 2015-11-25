809a90ec01c08790c30d5f7e1fcb39466fbf61c8
Yifan Y Liao <lyyf927@qq.com>
2015-11-25 16:44:16 +0800

    fix permission

--- studentgrading/users/models.py
+++ studentgrading/users/models.py
@@ -4,7 +4,7 @@ This file is almost a copy of https://github.com/fusionbox/django-authtools/blob
 """
 from __future__ import unicode_literals, absolute_import
 
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
@@ -20,20 +20,21 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, **kwargs):
         user = self.create_user(**kwargs)
-        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
         return user
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
 
     username = models.CharField(
         max_length=255,
         unique=True,
     )
-    is_admin = models.BooleanField(_('admin status'), default=False,
+    is_staff = models.BooleanField(_('staff status'), default=False,
                                    help_text=_('Designates whether the user can log into this admin '
                                    'site.'))
-    is_active = models.BooleanField(_('active'), default=False,
+    is_active = models.BooleanField(_('active'), default=True,
                                     help_text=_('Designates whether this user should be treated as '
                                     'active. Unselect this instead of deleting accounts.'))
     date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
