107f31f31dde91b18debda9199c0c59fbfa0e4bc
yifan-liao <lyyf927@qq.com>
2016-01-25 11:08:53 +0800

    fix prod setting

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,3 +2,8 @@
 '''
 Production Configurations
 '''
+from config.settings.common import *
+
+# SECRET CONFIGURATION
+# ------------------------------------------------------------------------------
+SECRET_KEY = env("SECRET_KEY")
